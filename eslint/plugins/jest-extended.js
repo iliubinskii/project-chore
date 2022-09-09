08f3cc50fde638e6d8adbdfba4479e57291a8c77
Ilia Liubinskii <admin.phpdev@gmail.com>
2022-09-09 15:17:33 +0300

    feat(eslint,jest-extended): Enabled "prefer-to-be-false" and "prefer-to-be-true" rules

--- eslint/plugins/jest-extended.js
+++ eslint/plugins/jest-extended.js
@@ -2,9 +2,5 @@ const { getAllRules } = require("./api");
 
 module.exports = {
   plugins: ["jest-extended"],
-  rules: {
-    ...getAllRules("eslint-plugin-jest-extended"),
-    "jest-extended/prefer-to-be-false": "off",
-    "jest-extended/prefer-to-be-true": "off"
-  }
+  rules: getAllRules("eslint-plugin-jest-extended")
 };
