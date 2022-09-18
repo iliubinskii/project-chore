a7bd4e7fd8472b8413cbf83bad4ffe8b70270586
Ilia Liubinskii <admin.phpdev@gmail.com>
2022-09-19 01:21:21 +0300

    chore(sonar): Configured for sonarcloud

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  collectCoverageFrom: ["!**"],
+  collectCoverageFrom: [
+    "{api,bin,commitlint,eslint,jest,package-json-lint,php-cs-fixer,phpstan,stylelint,tsconfig,typedoc}/**/*.{js,jsx,ts,tsx,vue}",
+    "!**/*.d.ts"
+  ],
   preset: "./jest/jest-preset",
   resolver: "./jest/resolver",
   testEnvironment: "./jest/environments/node",
