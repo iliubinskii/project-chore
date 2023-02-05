014468984515bbbb612345dded44f8050c23d305
Ilia Liubinskii <ilia.liubinskii@gmail.com>
2023-02-05 10:38:57 +0200

    feat(eslint): Edited "react/jsx-filename-extension" rule

--- eslint/plugins/react.js
+++ eslint/plugins/react.js
@@ -5,6 +5,7 @@ module.exports = {
   settings: { react: { version: "detect" } },
   rules: {
     ...getAllRules("eslint-plugin-react"),
+    "react/jsx-filename-extension": ["warn", { extensions: [".jsx", ".tsx"] }],
     "react/jsx-sort-default-props": "off",
     "react/jsx-space-before-closing": "off"
   }
