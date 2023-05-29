39eeadf086645e59b6f5b6d180fe5ac675347b53
Ilia Liubinskii <ilia.liubinskii@gmail.com>
2023-05-29 23:32:52 +0300

    feat(husky): Skip on a CI server

--- bin/prepare.js
+++ bin/prepare.js
@@ -1,7 +1,11 @@
 try {
   const husky = require("husky");
 
-  husky.install();
+  const isCi = require("is-ci");
+
+  if (isCi) {
+    // Running on a CI server
+  } else husky.install();
 } catch (e) {
   if (e.code === "MODULE_NOT_FOUND") {
     // Expected
