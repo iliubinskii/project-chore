8b9fdbece0165086fb9548d62d145c8617b99f60
Ilia Liubinskii <ilia.liubinskii@gmail.com>
2024-01-31 13:46:43 +0200

    feat: Target ES2020

--- eslint/index.js
+++ eslint/index.js
@@ -48,13 +48,13 @@ module.exports = {
   env: {
     browser: true,
     commonjs: true,
-    es2022: true,
+    es2020: true,
     node: true
   },
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: { jsx: true },
-    ecmaVersion: 2017,
+    ecmaVersion: 2020,
     project: "tsconfig.json",
     sourceType: "module"
   }
